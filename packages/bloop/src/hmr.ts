a27f0bd3809b247e4429e4bc47a31f7ceb99cbf7
Neil Sarkar <83109+neilsarkar@users.noreply.github.com>
2025-11-20 21:26:51 -0800

    Fix jsr complaint

--- packages/bloop/src/hmr.ts
+++ packages/bloop/src/hmr.ts
@@ -1,14 +1,21 @@
-import { mount, type MountOpts } from "./mount";
+import { type MountOpts, mount } from "./mount";
 import type { Runtime } from "./runtime";
 import { assert } from "./util";
 
-export async function handleUpdate(newModule: any, oldRuntime: Runtime, mountOptions?: Partial<MountOpts>) {
+export async function handleUpdate(
+  newModule: any,
+  oldRuntime: Runtime,
+  mountOptions?: Partial<MountOpts>,
+): Promise<Runtime> {
   assert(
     newModule?.game,
     `HMR: missing game export on module: ${JSON.stringify(newModule)}`,
   );
 
-  const { runtime } = await mount({hooks: (newModule.game as any).hooks, ...mountOptions});
+  const { runtime } = await mount({
+    hooks: (newModule.game as any).hooks,
+    ...mountOptions,
+  });
 
   const tape = oldRuntime.saveTape();
   const snapshot = oldRuntime.snapshot();
